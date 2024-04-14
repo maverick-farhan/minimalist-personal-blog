7f6e7bf3156a1300fb631c569f8454c6484a3630
maverick-farhan <mdfarhanofficial@gmail.com>
2024-04-14 13:42:21 +0530

    Removed ignore files

--- admin/delete.php
+++
@@ -1,9 +0,0 @@
-<?php
-include './config.php';
-$id = $_GET['id'];
-$sql = "delete from posts where pid={$id}";
-$result = mysqli_query($connect, $sql) or die("Query failed to fetch");
-
-header("Location: posts.php");
-mysqli_close($connect);
-?>
